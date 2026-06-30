b493ab5800743c8ae0a64c970873ea2bee187511
Sreehari Annam <sannam@wescodist.com>
2026-06-30 07:19:18 -0400

    feat: load first 80 cities from cities.csv

--- cities.js
+++ cities.js
@@ -1,5 +1,20 @@
-﻿const oregonCities = [
-  // paste first 80 cities from cities.csv here
+const oregonCities = [
+  "Albany", "Amity", "Antelope", "Ashland", "Astoria",
+  "Athena", "Aumsville", "Aurora", "Baker City", "Bandon",
+  "Banks", "Beatty", "Beavercreek", "Beaverton", "Bend",
+  "Blue River", "Boardman", "Bonanza", "Boring", "Broadbent",
+  "Brookings", "Brownsville", "Burns", "Canby", "Cannon Beach",
+  "Canyon City", "Carlton", "Central Point", "Clackamas", "Clatskanie",
+  "Cloverdale", "Colton", "Coos Bay", "Coquille", "Corbett",
+  "Cornelius", "Corvallis", "Cottage Grove", "Creswell", "Dallas",
+  "Damascus", "Dayton", "Depoe Bay", "Dundee", "Eagle Creek",
+  "Eagle Point", "Elgin", "Elkton", "Elmira", "Enterprise",
+  "Estacada", "Eugene", "Fairview", "Florence", "Forest Grove",
+  "Garibaldi", "Gaston", "Gervais", "Gilchrist", "Gladstone",
+  "Gleneden Beach", "Gold Beach", "Gold Hill", "Grand Ronde", "Grants Pass",
+  "Gresham", "Halsey", "Hammond", "Happy Valley", "Harrisburg",
+  "Heppner", "Hermiston", "Hillsboro", "Hood River", "Independence",
+  "Ione", "Jacksonville", "Jefferson", "John Day", "Joseph",
 ];
 
 const BATCH_SIZE = 5;
