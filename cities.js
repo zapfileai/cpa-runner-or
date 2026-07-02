4a76431c7ebcb9f9ca6d4546ea7355194fdbb0fd
Sreehari Annam <sannam@wescodist.com>
2026-07-02 09:56:21 -0400

    batch 3: next cities

--- cities.js
+++ cities.js
@@ -1,20 +1,10 @@
 const oregonCities = [
-  "Junction City", "Keizer", "Keno", "Klamath Falls", "La Grande",
-  "La Pine", "Lafayette", "Lake Oswego", "Lakeside", "Lakeview",
-  "Lebanon", "Lexington", "Lincoln City", "Lowell", "Madras",
-  "Malin", "Manzanita", "Mapleton", "Marcola", "Mcminnville",
-  "Medford", "Merrill", "Mill City", "Milton Freewater", "Molalla",
-  "Monmouth", "Monroe", "Mount Angel", "Mount Hood Parkdale", "Mulino",
-  "Myrtle Creek", "Myrtle Point", "Neskowin", "Newberg", "Newport",
-  "North Bend", "North Plains", "Oakridge", "Oceanside", "Odell",
-  "Ontario", "Oregon City", "Otis", "Pacific City", "Pendleton",
-  "Philomath", "Phoenix", "Pleasant Hill", "Port Orford", "Portland",
-  "Powell Butte", "Prineville", "Rainier", "Redmond", "Reedsport",
-  "Rhododendron", "Rickreall", "Rockaway Beach", "Rogue River", "Roseburg",
-  "Saint Helens", "Saint Paul", "Salem", "Sandy", "Scappoose",
-  "Scio", "Seaside", "Shady Cove", "Sheridan", "Sherwood",
-  "Silverton", "Sisters", "South Beach", "Springfield", "Stayton",
-  "Sublimity", "Summer Lake", "Sutherlin", "Sweet Home", "Talent",
+  "Terrebonne", "The Dalles", "Tillamook", "Troutdale", "Tualatin",
+  "Turner", "Umatilla", "Umpqua", "Union", "Veneta",
+  "Vernonia", "Waldport", "Walterville", "Warren", "Warrenton",
+  "Welches", "West Linn", "Weston", "White City", "Wilbur",
+  "Willamina", "Wilsonville", "Winchester", "Woodburn", "Yachats",
+  "Yamhill",
 ];
 
 const BATCH_SIZE = 5;
